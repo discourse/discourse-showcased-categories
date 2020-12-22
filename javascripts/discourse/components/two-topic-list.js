2e46e446acbc4d3ff1c458fb6ca934e7cf488671
Jordan Vidrine <jordan@jordanvidrine.com>
2020-12-22 11:28:52 -0600

    Refactor
    This commit adds the refactor work David implemented.

--- javascripts/discourse/components/two-topic-list.js
+++ javascripts/discourse/components/two-topic-list.js
@@ -1,131 +1,31 @@
 import Component from "@ember/component";
-import { ajax } from "discourse/lib/ajax";
-import Topic from 'discourse/models/topic';
-import { action } from "@ember/object";
 import Category from "discourse/models/category";
 import discourseComputed from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+import { and } from "@ember/object/computed";
 
 export default Component.extend({
-  topicListOne: null,
-  topicListTwo: null,
   router: service(),
 
-  init() {
-    this._super(...arguments);
-    
-    if (Category.list().length == 0) return false;
-
-    let categoryOne = Category.findById(settings.feed_one_category);
-    let hrefOne = 'c/';
-
-    if (!categoryOne) return false;
-
-    if (categoryOne.parentCategory) {
-      hrefOne += categoryOne.parentCategory.slug + "/" + categoryOne.slug + ".json";
-    } else {
-      hrefOne += categoryOne.slug + ".json";
-    }
-
-    ajax(hrefOne).then((result) => {
-      let topicList = [];
-      result.topic_list.topics.slice(0,settings.max_list_length).forEach((topic) => {
-        topicList.push(Topic.create(topic));
-      });
-      if (topicList.length !== 0) {
-        this.set("topicListOne", topicList);
-      }
-    });
-
-    let categoryTwo = Category.findById(settings.feed_two_category);
-    let hrefTwo = 'c/';
-
-    if (!categoryTwo) return false;
-
-    if (categoryTwo.parentCategory) {
-      hrefTwo += categoryTwo.parentCategory.slug + "/" + categoryTwo.slug + ".json";
-    } else {
-      hrefTwo += categoryTwo.slug + ".json";
-    }
-
-    ajax(hrefTwo).then((result) => {
-      let topicList = [];
-      result.topic_list.topics.slice(0,settings.max_list_length).forEach((topic) => {
-        topicList.push(Topic.create(topic));
-      });
-      if (topicList.length !== 0) {
-        this.set("topicListTwo", topicList);
-      }
-    });
-
+  get categoriesLoaded() {
+    return Category.list().length !== 0;
   },
 
-  @discourseComputed()
-  topicListOneHref() {
-    if (Category.list().length == 0) return false;
-
-    let category = Category.findById(settings.feed_one_category);
-
-    if (category) {
-      if (category.parentCategory) {
-        let parent = Category.findById(category.parentCategory.id)
-        return parent.slug + "/" + category.slug;
-      } else {
-        return category.slug;
-      }
-    }
+  get category1() {
+    if (!this.categoriesLoaded) return false;
+    return Category.findById(settings.feed_one_category);
   },
 
-  @discourseComputed()
-  topicListTwoHref() {
-    if (Category.list().length == 0) return false;
-
-    let category = Category.findById(settings.feed_two_category);
-
-    if (category) {
-      if (category.parentCategory) {
-        let parent = Category.findById(category.parentCategory.id)
-        return parent.slug + "/" + category.slug;
-      } else {
-        return category.slug;
-      }
-    }
+  get category2() {
+    if (!this.categoriesLoaded) return false;
+    return Category.findById(settings.feed_two_category);
   },
 
-  @discourseComputed("router.currentURL")
-  showTopicLists(currentURL) {
-    if (settings.feed_one_category && settings.feed_two_category && Category.list().length !== 0) {
-      return currentURL.split("?")[0] === "/";
-    } else {
-      return false;
-    }
+  @discourseComputed("router.currentRouteName")
+  isHomepage(currentRouteName) {
+    return currentRouteName == `discovery.${defaultHomepage()}`;
   },
 
-  @action
-  createListOneTopic() {
-    if (Discourse.User.current()) {
-      Discourse.__container__.lookup('controller:composer')
-      .open({
-        action: "createTopic", 
-        draftKey: "createTopic", 
-        categoryId: settings.feed_one_category
-      });
-    } else {
-      return window.location = getURL("/signup");
-    }
-  },
-
-  @action
-  createListTwoTopic() {
-    if (Discourse.User.current()) {
-      Discourse.__container__.lookup('controller:composer')
-      .open({
-        action: "createTopic", 
-        draftKey: "createTopic", 
-        categoryId: settings.feed_two_category
-      });
-    } else {
-      return window.location = getURL("/signup");
-    }
-  }
-})
+  showTopicLists: and("isHomepage", "category1", "category2"),
+});
