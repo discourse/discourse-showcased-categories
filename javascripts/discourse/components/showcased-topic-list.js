848af8bcd7ef6f2246f4c31422c8b6e657459ea7
Jordan Vidrine <30537603+jordanvidrine@users.noreply.github.com>
2020-12-22 14:05:54 -0600

    Refactor (#6)
    * SYNTAX: Remove unneeded grep lines
    
    * Refactor
    
    This commit adds the refactor work David implemented.
    
    * SYNTAX: Rename folder, + file
    
    This commit renames a folder and its containing file to match the theme-component name.

---
+++ javascripts/discourse/components/showcased-topic-list.js
@@ -0,0 +1,45 @@
+import Component from "@ember/component";
+import { ajax } from "discourse/lib/ajax";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { readOnly } from "@ember/object/computed";
+
+export default Component.extend({
+  moreHref: readOnly("category.url"),
+  router: service(),
+
+  init() {
+    this._super(...arguments);
+
+    if (!this.category) return;
+
+    const filter = {
+      filter: "latest",
+      params: {
+        category: this.category.id,
+      },
+    };
+
+    this.store.findFiltered("topicList", filter).then((topicList) => {
+      this.set("topicList", topicList.topics.slice(0, 5));
+    });
+  },
+
+  @action
+  createTopic() {
+    if (this.currentUser) {
+      // This is not ideal - we should not be using __container__ here
+      // We can't inject it properly, because ember doesn't allow injecting controllers into components
+      // We can't `sendAction` up to routes/application createNewTopicViaParams because only clojure actions are allowed
+      // We can't use clojure actions because then an openComposer action would have to be passed to every plugin outlet
+      // The best solution is probably a core appEvent or service which themes could trigger
+      Discourse.__container__.lookup("controller:composer").open({
+        action: "createTopic",
+        draftKey: "createTopic",
+        categoryId: this.category.id,
+      });
+    } else {
+      this.router.transitionTo("login");
+    }
+  },
+});
