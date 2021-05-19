e192d8226a17764bd83e5687905197a64884078e
Jordan Vidrine <30537603+jordanvidrine@users.noreply.github.com>
2021-05-19 11:48:48 -0400

    Sidebar addition (#8)
    * FEATURE: Add ability to show showcased topiclist as a sidebar

--- javascripts/discourse/components/two-topic-list.js
+++ javascripts/discourse/components/two-topic-list.js
@@ -1,12 +1,32 @@
 import Component from "@ember/component";
 import Category from "discourse/models/category";
-import discourseComputed from "discourse-common/utils/decorators";
+import discourseComputed, { observes } from "discourse-common/utils/decorators";
 import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 import { and } from "@ember/object/computed";
 
 export default Component.extend({
   router: service(),
+  tagName: "",
+
+  didInsertElement() {
+    this._super(...arguments);
+    this._updateBodyClasses();
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    this._updateBodyClasses();
+  },
+
+  @observes("shouldShow")
+  _updateBodyClasses() {
+    const shouldCleanup = this.isDestroying || this.isDestroyed;
+    if (!shouldCleanup && this.shouldShow && settings.show_as_sidebar) {
+      document.body.classList.add("showcased-categories-sidebar");
+    } else {
+      document.body.classList.remove("showcased-categories-sidebar");
+    }
+  },
 
   get categoriesLoaded() {
     return Category.list().length !== 0;
@@ -23,9 +43,11 @@ export default Component.extend({
   },
 
   @discourseComputed("router.currentRouteName")
-  isHomepage(currentRouteName) {
-    return currentRouteName == `discovery.${defaultHomepage()}`;
+  shouldShow(currentRouteName) {
+    let showSidebar =
+      settings.show_as_sidebar && currentRouteName === "discovery.latest";
+    return currentRouteName === `discovery.${defaultHomepage()}` || showSidebar;
   },
 
-  showTopicLists: and("isHomepage", "category1", "category2"),
+  showTopicLists: and("shouldShow", "category1", "category2")
 });
