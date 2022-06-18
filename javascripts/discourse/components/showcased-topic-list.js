b801174fd51cecb50146bd7e8d9ec33ab1c47855
Jarek Radosz <jradosz@gmail.com>
2022-06-18 21:22:58 +0200

    DEV: Add CI setup and fix linting issues (#15)

--- javascripts/discourse/components/showcased-topic-list.js
+++ javascripts/discourse/components/showcased-topic-list.js
@@ -1,8 +1,7 @@
 import Component from "@ember/component";
-import { ajax } from "discourse/lib/ajax";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 import { readOnly } from "@ember/object/computed";
+import { getOwner } from "discourse-common/lib/get-owner";
 
 export default Component.extend({
   moreHref: readOnly("category.url"),
@@ -10,7 +9,9 @@ export default Component.extend({
   init() {
     this._super(...arguments);
 
-    if (!this.category) return;
+    if (!this.category) {
+      return;
+    }
 
     const filter = {
       filter: "latest",
@@ -19,7 +20,7 @@ export default Component.extend({
       },
     };
 
-    this.set("isLoading", true)
+    this.set("isLoading", true);
 
     this.store.findFiltered("topicList", filter).then((topicList) => {
       this.set(
@@ -27,19 +28,14 @@ export default Component.extend({
         topicList.topics.slice(0, settings.max_list_length)
       );
 
-      this.set("isLoading", false)
+      this.set("isLoading", false);
     });
   },
 
   @action
   createTopic() {
     if (this.currentUser) {
-      // This is not ideal - we should not be using __container__ here
-      // We can't inject it properly, because ember doesn't allow injecting controllers into components
-      // We can't `sendAction` up to routes/application createNewTopicViaParams because only clojure actions are allowed
-      // We can't use clojure actions because then an openComposer action would have to be passed to every plugin outlet
-      // The best solution is probably a core appEvent or service which themes could trigger
-      Discourse.__container__.lookup("controller:composer").open({
+      getOwner(this).lookup("controller:composer").open({
         action: "createTopic",
         draftKey: "createTopic",
         categoryId: this.category.id,
--- javascripts/showcased-topic-list/connectors/above-main-container/showcased-topic-list.js
+++ javascripts/showcased-topic-list/connectors/above-main-container/showcased-topic-list.js
@@ -1,6 +1,6 @@
 export default {
-  setupComponent(args, component) {
-    if (settings.plugin_outlet == "above-main-container") {
+  setupComponent() {
+    if (settings.plugin_outlet === "above-main-container") {
       this.set("aboveMain", true);
     } else {
       this.set("aboveMain", false);
